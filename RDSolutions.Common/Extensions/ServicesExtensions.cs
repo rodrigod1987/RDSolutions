85681293b253203e1120d2969eebff5a5dc29c43
Rodrigo Duarte <rodrigo.duarte@take.net>
2023-12-17 23:29:12 -0300

    Adjus loading assembly method.

--- RDSolutions.Common/Extensions/ServicesExtensions.cs
+++ RDSolutions.Common/Extensions/ServicesExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 
@@ -11,16 +13,21 @@ public static class ServicesExtensions
     {
         //Acessar todas as dlls procurando por aquelas que implementam IRegistrable e chamar o m�todo Register passando 
         //o argumento services. Com isso consigo isolar em cada um dos m�dulos aquilo que eu devo injetar como depend�ncia.
-        var assemblies = Assembly
-            .GetCallingAssembly()
-            .GetReferencedAssemblies()
-            .Where(x => x.Name.StartsWith(namespaceStartWith))
+        var allAssemblies = new List<Assembly>();
+        var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        var files = Directory.GetFiles(path, "*.dll");
+
+        foreach (string dll in files) 
+        {
+            allAssemblies.Add(Assembly.LoadFile(dll));
+        }
+
+        var assemblies = allAssemblies
+            .Where(x => x.GetName().Name.StartsWith(namespaceStartWith))
             .ToList();
 
-        foreach (var assemblyName in assemblies)
+        foreach (var assembly in assemblies)
         {
-            Assembly assembly = Assembly.Load(assemblyName);
-
             var myTypes = assembly
                 .GetTypes()
                 .Where(mytype => mytype.GetInterfaces().Contains(typeof(IRegistrable)));
